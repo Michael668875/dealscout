d781608cd19b46d91d4a6dc5a2ad4ba57c5e9000
Michael668875 <159018843+Michael668875@users.noreply.github.com>
2026-07-01 20:12:11 +1000

    added all models to admin. setting up templates

--- dealscout/keyboard_tracker/admin.py
+++ dealscout/keyboard_tracker/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
+from .models import CanonBrand, CanonModel, Product, Listing, PriceHistory, ModelAlias, TempSummary
+
 # Register your models here.
+
+class ListingAdmin(admin.ModelAdmin):
+    search_fields = ["title"]
+    list_filter = ["country"]
+
+admin.site.register(Listing, ListingAdmin)
+admin.site.register(CanonBrand)
+admin.site.register(CanonModel)
+admin.site.register(Product)
+admin.site.register(PriceHistory)
+admin.site.register(ModelAlias)
+admin.site.register(TempSummary)
--- dealscout/keyboard_tracker/views.py
+++ dealscout/keyboard_tracker/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Listing
 
 # Create your views here.
+
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
+
+def listings(request):
+    listings = Listing.objects.all()
+    return render(request, "keyboard_tracker/listings.html", {
+        "listings": listings,
+    })
