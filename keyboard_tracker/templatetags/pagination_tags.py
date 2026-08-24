d1a2339650f8c3480afc7f0dda0e71b271269375
TPt480 <159018843+Michael668875@users.noreply.github.com>
2026-08-24 21:33:49 +1000

    fixed pagination

--- keyboard_tracker/templatetags/pagination_tags.py
+++ keyboard_tracker/templatetags/pagination_tags.py
@@ -5,4 +5,25 @@ register = template.Library()
 
 @register.simple_tag
 def elided_page_range(page_obj):
-    return page_obj.paginator.get_elided_page_range(page_obj.number)
+    paginator = page_obj.paginator
+    current = page_obj.number
+    total = paginator.num_pages
+
+    if total <= 7:
+        return range(1, total + 1)
+
+    page_range = [1]
+
+    if current > 3:
+        page_range.append(paginator.ELLIPSIS)
+
+    for page in range(max(2, current - 1), min(total, current + 1) + 1):
+        page_range.append(page)
+
+    if current < total - 2:
+        page_range.append(paginator.ELLIPSIS)
+
+    if total not in page_range:
+        page_range.append(total)
+
+    return page_range
